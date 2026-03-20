a3d4d1fcef66849bee7e8dd6abff4d9cf26ae71a
tatiane-oliveiraa <materialparaestudoti@gmail.com>
2026-03-20 08:46:23 -0300

    feat: atualizando URL do backend para o deploy no render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // URL do seu FastAPI
+  baseURL: 'https://cadastro-usuarios-crud.onrender.com', // URL do seu FastAPI
 });
 
 export default api;
